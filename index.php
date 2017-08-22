ebb5fa72faea0789db83b1c02e165f802f37ba8b
lm815 <maria2704@inbox.ru>
2017-08-22 19:30:51 +0300

    Подключили стили bootstrap

--- index.php
+++ index.php
@@ -1,20 +1,29 @@
 <?php
 
-$message = false;
+$message = false; //
+$error = false; //Для ошибок
 
 if (isset($_REQUEST['name']) and isset($_REQUEST['phone'])) {
 
-    $name = $_REQUEST['name'];
+    $name = $_REQUEST['name']; // Для сокращения
     $phone = $_REQUEST['phone'];
 
-    $row = 'Здравствуйте, ' . $name .
-        '. Ваш номер: ' . $phone . PHP_EOL;
+    if (empty($name) || empty($phone)) {
+        $error = 'Заполните поля';
+    } else {
+        $row = 'Здравствуйте, ' . $name .
+            '. Ваш номер: ' . $phone . PHP_EOL;
 
-    file_put_contents('./contacts.txt',
-        $row, FILE_APPEND);
+        // PHP_EOL = '\n'
 
-    $message = 'Спасибо! Мы с Вами свяжемся.';
+        file_put_contents('./contacts.txt',
+            $row, FILE_APPEND);
+
+        $message = 'Спасибо! Мы с Вами свяжемся.';
+    }
+
 }
+
 ?>
 
 <!doctype html>
@@ -22,20 +31,40 @@ if (isset($_REQUEST['name']) and isset($_REQUEST['phone'])) {
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <link rel="stylesheet" href="./CSS/bootstrap.min.css">
 </head>
 <body>
 
+   <div class="container">
+
+       <h3>Форма обратной связи</h3>
+
 <?php if ($message) : ?>
-   <?= $message ?>
+    <p class="alert-success col-md-4"><?= $message ?></p>
+
 <?php else: ?>
-       <form action="index.php" method="post">
-    <p>Представьтесь</p>
-    <input type="text" name="name">
-    <p>Укажите ваш номер</p>
-    <input type="text" name="phone">
-    <button type="submit">Отправить</button>
-       </form>
+    <form class="form-horizontal" action="index.php" method="post">
+        <div class="form-group">
+            <label for="name" class="col-sm-2 control-label">Имя</label>
+            <div class="col-md-4">
+                <input type="text" name="name" class="form-control" placeholder="Имя">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="phone" class="col-sm-2 control-label">Номер телефона</label>
+            <div class="col-md-4">
+                <input type="text" name="phone" class="form-control" placeholder="Введите номер">
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="col-sm-offset-2 col-md-10">
+                <button type="submit" class="btn btn-primary">Отправить</button>
+            </div>
+        </div>
+        <p class="alert-danger col-md-4"><?= $error ?></p>
+    </form>
 <?php endif; ?>
 
+     </div>
 </body>
 </html>
