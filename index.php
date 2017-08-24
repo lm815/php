3a0a6f810eb8142c6ee06a30e8f6dddb57ed6e01
lm815 <maria2704@inbox.ru>
2017-08-24 19:31:25 +0300

    Добавлена кнопка сбросить

--- index.php
+++ index.php
@@ -34,59 +34,67 @@ if (isset($_REQUEST['name']) and isset($_REQUEST['phone'])) {
     <?php if ($message) : ?>
         <p class="alert-success col-md-4"><?= $message ?></p>
     <?php else: ?>
-        <form class="form-horizontal" action="index.php" method="post">
-            <div class="form-group">
-                <label for="name" class="col-sm-2 control-label">Имя</label>
-                <div class="col-md-4">
-                    <input type="text" name="name" class="form-control" placeholder="Имя" required>
-                </div>
-                <form class="form-horizontal" action="index.php" method="post">
+    <form class="form-horizontal" action="index.php" method="post">
+        <div class="form-group">
+            <label for="name" class="col-sm-2 control-label">Имя</label>
+            <div class="col-md-4">
+                <input type="text" name="name" class="form-control" placeholder="Имя" required>
+            </div>
+            <form class="form-horizontal" action="index.php" method="post">
+                <div class="form-group">
+                    <label for="surname" class="col-sm-2 control-label">Фамилия</label>
+                    <div class="col-md-4">
+                        <input type="text" name="surname" class="form-control" placeholder="Фамилия" required>
+                    </div>
                     <div class="form-group">
-                        <label for="surname" class="col-sm-2 control-label">Фамилия</label>
+                        <label for="pol" class="col-sm-2">Пол</label>
                         <div class="col-md-4">
-                            <input type="text" name="surname" class="form-control" placeholder="Фамилия" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="pol" class="col-sm-2">Пол</label>
-                            <div class="col-md-4">
-                        <select name="pol" id="pol">
-                            <option value="">Указать</option>
-                            <option value="1">Мужской</option>
-                            <option value="2">Женский</option>
-                        </select>
+                            <select name="pol" id="pol">
+                                <option value="">Указать</option>
+                                <option value="1">Мужской</option>
+                                <option value="2">Женский</option>
+                            </select>
                         </div>
                         <form class="form-horizontal" action="index.php" method="post">
                             <div class="form-group">
                                 <label for="city" class="col-sm-2 control-label">Город проживания</label>
                                 <div class="col-md-4">
-                                    <input type="text" name="city" class="form-control" placeholder="Город проживания" required>
+                                    <input type="text" name="city" class="form-control" placeholder="Город проживания"
+                                           required>
                                 </div>
                                 <form class="form-horizontal" action="index.php" method="post">
-            <div class="form-group">
-                <label for="phone" class="col-sm-2 control-label">Номер телефона</label>
-                <div class="col-md-4">
-                    <input type="text" name="phone" class="form-control" placeholder="Введите номер" required>
-                </div>
-            </div>
-            <form class="form-horizontal" action="index.php" method="post">
-                <div class="form-group">
-                    <label for="email" class="col-sm-2" control-label">email</label>
-                <div class="col-md-4">
-                    <input type="text" name="email" class="form-control" placeholder="email" required>
-                </div>
-                    <p>
-            <div class="form-group">
-                <div class="col-sm-offset-2 col-md-10">
-                    <button type="submit" class="btn btn-primary">Отправить</button>
-                </div>
-                    </div>
-                </p>
-            <p class="alert-danger col-md-4"><?= $error ?></p>
-        </form>
-    <?php endif; ?>
+                                    <div class="form-group">
+                                        <label for="phone" class="col-sm-2 control-label">Номер телефона</label>
+                                        <div class="col-md-4">
+                                            <input type="text" name="phone" class="form-control"
+                                                   placeholder="Введите номер" required>
+                                        </div>
+                                    </div>
+                                    <form class="form-horizontal" action="index.php" method="post">
+                                        <div class="form-group">
+                                            <label for="email" class="col-sm-2 control-label">e-mail</label>
+                                            <div class="col-md-4">
+                                                <input type="text" name="email" class="form-control"
+                                                       placeholder="email" required>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="col-sm-offset-2 col-md-10">
+                                                <button type="submit" class="btn btn-primary">Отправить</button>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="col-sm-offset-2 col-md-10">
+                                                <button type="reset" class="btn btn-primary">Сбросить</button>
+                                            </div>
+                                        </div>
+
+                                        <p class="alert-danger col-md-4"><?= $error ?></p>
+                                    </form>
+                                    <?php endif; ?>
 
 
-</div>
+                            </div>
 
 </body>
 </html>
