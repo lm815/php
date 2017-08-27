c746e15efc80f70e5c6cd178c09c2d3e45a40e95
itsme815 <18andreass@gmail.com>
2017-08-27 13:53:20 +0300

    Радиокнопка, макс.кол.символов и проверка телефона

--- index.php
+++ index.php
@@ -24,27 +24,34 @@ if (isset($_REQUEST['name']) and isset($_REQUEST['phone'])) {
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
-    <link rel="stylesheet" href="./CSS/bootstrap.min.css">
+    <link rel="stylesheet" href="./CSS/bootstrap.min.css" type="text/css">
 </head>
 <body>
-
 <div class="container">
     <h3>Заявка на участие в соревнованиях</h3>
-
     <?php if ($message) : ?>
         <p class="alert-success col-md-4"><?= $message ?></p>
     <?php else: ?>
     <form class="form-horizontal" action="index.php" method="post">
+        <div class="form-group"></div>
+        <div class="col-md-4">
+            <p><b>Какой вид спорта Вы выбираете?</b></p>
+            <p><input name="view" type="radio" value="swimming"> Плавание</p>
+            <p><input name="view" type="radio" value="run"> Бег</p>
+            <p><input name="view" type="radio" value="jumps" checked> Прыжки в длину</p>
+        </div>
+    </form>
+    <form class="form-horizontal" action="index.php" method="post">
         <div class="form-group">
             <label for="name" class="col-sm-2 control-label">Имя</label>
             <div class="col-md-4">
-                <input type="text" name="name" class="form-control" placeholder="Имя" required>
+                <input type="text"  name="name" maxlength="10" class="form-control" placeholder="Имя" required>
             </div>
             <form class="form-horizontal" action="index.php" method="post">
                 <div class="form-group">
                     <label for="surname" class="col-sm-2 control-label">Фамилия</label>
                     <div class="col-md-4">
-                        <input type="text" name="surname" class="form-control" placeholder="Фамилия" required>
+                        <input type="text" name="surname" maxlength="15" class="form-control" placeholder="Фамилия" required>
                     </div>
                     <div class="form-group">
                         <label for="pol" class="col-sm-2">Пол</label>
@@ -59,14 +66,14 @@ if (isset($_REQUEST['name']) and isset($_REQUEST['phone'])) {
                             <div class="form-group">
                                 <label for="city" class="col-sm-2 control-label">Город проживания</label>
                                 <div class="col-md-4">
-                                    <input type="text" name="city" class="form-control" placeholder="Город проживания"
+                                    <input type="text" name="city" maxlength="15" class="form-control" placeholder="Город проживания"
                                            required>
                                 </div>
                                 <form class="form-horizontal" action="index.php" method="post">
                                     <div class="form-group">
                                         <label for="phone" class="col-sm-2 control-label">Номер телефона</label>
                                         <div class="col-md-4">
-                                            <input type="text" name="phone" class="form-control"
+                                            <input type="text" name="phone" pattern="[0-9]{5,10}" class="form-control"
                                                    placeholder="Введите номер" required>
                                         </div>
                                     </div>
@@ -74,7 +81,7 @@ if (isset($_REQUEST['name']) and isset($_REQUEST['phone'])) {
                                         <div class="form-group">
                                             <label for="email" class="col-sm-2 control-label">e-mail</label>
                                             <div class="col-md-4">
-                                                <input type="text" name="email" class="form-control"
+                                                <input type="email" name="email" class="form-control"
                                                        placeholder="email" required>
                                             </div>
                                         </div>
@@ -95,6 +102,5 @@ if (isset($_REQUEST['name']) and isset($_REQUEST['phone'])) {
 
 
                             </div>
-
 </body>
 </html>
